bb669ee365bb150c58d5629036b0447dedb5897e
Eric Tzimas <errikos.tzimas@gmail.com>
2020-10-30 01:26:09 +0200

    Improved GUI, implemented a stop button

--- timer2.py
+++ timer2.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.ttk import *
+
 import time
 import os
 from time import strftime 
@@ -8,72 +8,70 @@ NORM_FONT = ("Helvetica", 10)
 SMALL_FONT = ("Helvetica", 8)
 global c
 c = -1
-global togg 
-togg = FALSE
-
-
-
-
-
+global count
+global solve
+count = 0
+counti = 0
 def clicked():
-    
-   
-   
+    global count
     global secs
-    secs=int(te.get())
+    count += 1
+    secs=int(hms_to_seconds(te.get()))
     global cii
-    cii = int(te.get())
-    
-
-    
+    cii = int(hms_to_seconds(te.get())) 
     lbl2.pack()
     time2()
 def clicked2():
-
+    global solve
     time()
     lbl.pack(anchor = 'center')
-    
- 
-
-
 def time():
     string = strftime('%H:%M:%S %p') 
     lbl.config(text = string) 
-    lbl.after(1000, time)
     
+    lbl.after(1000, time)
 
 def time2():
     global cii
+    global solve
+    k=0
     
     lbl2.config(text = convert(cii))
     cii = cii - 1
     if cii==-1:
-        popupmsg("Done")
-    lbl2.after(1000,time2)
+        popupmsg("Time is up!")
     
 
+    solve = lbl2.after(1000,time2)
+    
+    
+def stop():
+    global solve
+    if solve:
+        lbl2.after_cancel(solve)
+       
+     
+   
+     
+    
+    
 
-
-
-
-
-
-
-
-
-
-    togg = True
 def popupmsg(msg):
     global c
     popup = Tk()
+    popup.resizable(False,False)
     popup.geometry("200x100")
     popup.wm_title("Alarm")
-    label = Label(popup, text=msg, font=NORM_FONT)
+    label = Label(popup, text=msg, font=(NORM_FONT, 30))
+    label.config(width=25)
     os.system('say "Time is up"')
     c = 0
 
     B1 = Button(popup, text="Okay", command = popup.destroy)
-    B1.pack(anchor = 'center')
+    B1.config(width=10)
+    label.pack()
+
+    B1.pack()
     popup.mainloop()       
         
 def convert(seconds): 
@@ -84,38 +82,45 @@ def convert(seconds):
     seconds %= 60
       
     return "%d:%02d:%02d" % (hour, minutes, seconds)        
-        
+def hms_to_seconds(t):
+    h, m, s = [int(i) for i in t.split(':')]
+    return 3600*h + 60*m + s        
 
 root = Tk()
 root.title('Alarm')
+w = Frame(root)
+
+w.pack()
 
 
-root.geometry("400x200")
-lbl = Label( root,font = ('calibri', 40, 'bold'), 
+root.geometry("400x300")
+lbl = Label(w,font = ('calibri', 40, 'bold'), 
             background = 'white', 
             foreground = 'black')
-lbl.config(background='snow')
-lbl2 = Label( root,font = ('calibri', 40, 'bold'), 
+lbl.config(background='snow',fg='grey')
+lbl2 = Label(w,font = ('calibri', 40, 'bold'), 
             background = 'white', 
-            foreground = 'black')  
-button = Button(command = clicked,text = "Start")
-button2 = Button(command = clicked2,text = "Clock")
+            foreground = 'red')  
+button = Button(w,command = clicked,text = "Start")
+button.config(width = 20)
+button2 = Button(w,command = clicked2,text = "Clock")
+button2.config(width = 20)
+button3 = Button(w, command = stop ,text = 'Stop')
+button3.config(width=20)
 
-te = Entry(text = "time in minutes")
-
+te = Entry(w,text = "time in minutes")
 
 te.config(background='snow')
 
-
-lab = Label(text = 'Enter time in seconds')
+lab = Label(w,text = 'Enter time in seconds')
 lab.config(background='snow')
 
 lab.pack()
 te.pack()
 
-
 button2.pack()
 button.pack()
-
+button3.pack()
+root.resizable(False,False)
 
 mainloop()
